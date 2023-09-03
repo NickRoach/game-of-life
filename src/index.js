e13c25ac158465e5f468f4ae1861c7703b8e1c46
Nick Roach <njlroach@gmail.com>
2023-09-03 20:06:32 +1000

    initial commit

---
+++ src/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+exports.__esModule = true;
+var lodash_1 = __importDefault(require("lodash"));
+function component() {
+    var element = document.createElement("div");
+    // Lodash, now imported by this script
+    element.innerHTML = lodash_1["default"].join(["Hello", "webpackzz"], " ");
+    return element;
+}
+document.body.appendChild(component());
