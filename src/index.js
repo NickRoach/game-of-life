3f178f8abd37e1d597775823e4796d62b011f3c0
NickRoach <njlroach@gmail.com>
2023-09-04 17:16:31 +1000

    cellular automata game of life working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
-var lineLength = window.innerWidth;
+var lineLength = window.innerWidth / 3;
 var start;
-var canvas, ctx;
+var canvas;
+var ctx;
 var lastFrame = 0;
 var row = 0;
-var backgroundColor = "#222222";
+var backgroundColor = "#000000";
+var cellColor = "turquoise";
 var direction = "down";
+var showGrid = false;
+var boxSize = 3;
+var boxes = [[], []];
 var resizeCanvas = function () {
     canvas.height = window.innerHeight - 4;
     canvas.width = window.innerWidth;
@@ -15,28 +20,123 @@ var clear = function () {
     ctx.fillStyle = backgroundColor;
     ctx.fill();
 };
+var renderGrid = function () {
+    if (!showGrid)
+        return;
+    ctx.lineWidth = 1;
+    ctx.strokeStyle = "#999999";
+    // draw vertical lines
+    for (var i = 0; i <= canvas.width; i += boxSize) {
+        ctx.beginPath();
+        ctx.moveTo(i, 0);
+        ctx.lineTo(i, canvas.height);
+        ctx.stroke();
+    }
+    // draw horizontal lines
+    for (var i = 0; i < canvas.height; i += boxSize) {
+        ctx.beginPath();
+        ctx.moveTo(0, i);
+        ctx.lineTo(canvas.width, i);
+        ctx.stroke();
+    }
+};
+var getNeighbors = function (boxes, x, y) {
+    var counter = 0;
+    // left top
+    if (x > 0 && y > 0 && boxes[x - 1][y - 1])
+        counter++;
+    // left middle
+    if (x > 0 && y < boxes[x].length - 1 && boxes[x - 1][y])
+        counter++;
+    // left bottom
+    if (x > 0 && boxes[x - 1][y + 1])
+        counter++;
+    // middle top
+    if (y > 0 && boxes[x][y - 1])
+        counter++;
+    // middle bottom
+    if (y < boxes[x].length - 1 && boxes[x][y + 1])
+        counter++;
+    // right top
+    if (y > 0 && x < boxes.length - 1 && boxes[x + 1][y - 1])
+        counter++;
+    // right middle
+    if (x < boxes.length - 1 && boxes[x + 1][y])
+        counter++;
+    // right bottom
+    if (x < boxes.length - 1 && y < boxes[x].length - 1 && boxes[x + 1][y + 1])
+        counter++;
+    return counter;
+};
+var setBoxes = function () {
+    var xBoxes = Math.floor(canvas.width / boxSize);
+    var yBoxes = Math.floor(canvas.height / boxSize);
+    for (var x = 0; x <= xBoxes; x++) {
+        boxes[x] = [];
+        for (var y = 0; y <= yBoxes; y++) {
+            boxes[x][y] = false;
+            if (Math.random() > 0.9)
+                boxes[x][y] = true;
+        }
+    }
+    // // boxes[Math.floor(xBoxes / 2) - 1][Math.floor(yBoxes / 2) - 1] = true
+    // // boxes[Math.floor(xBoxes / 2) - 1][Math.floor(yBoxes / 2)] = true
+    // boxes[Math.floor(xBoxes / 2) - 1][Math.floor(yBoxes / 2) + 1] = true
+    // boxes[Math.floor(xBoxes / 2)][Math.floor(yBoxes / 2) - 1] = true
+    // // boxes[Math.floor(xBoxes / 2)][Math.floor(yBoxes / 2)] = true
+    // boxes[Math.floor(xBoxes / 2)][Math.floor(yBoxes / 2) + 1] = true
+    // // boxes[Math.floor(xBoxes / 2) + 1][Math.floor(yBoxes / 2) - 1] = true
+    // boxes[Math.floor(xBoxes / 2) + 1][Math.floor(yBoxes / 2)] = true
+    // boxes[Math.floor(xBoxes / 2) + 1][Math.floor(yBoxes / 2) + 1] = true
+};
+var calculateNewBoxes = function () {
+    var newBoxes = [];
+    // fill newBoxes with false
+    for (var i = 0; i < boxes.length; i++) {
+        newBoxes.push([]);
+        for (var j = 0; j < boxes[i].length; j++)
+            newBoxes[i].push(false);
+    }
+    for (var x = 0; x < boxes.length; x++) {
+        for (var y = 0; y < boxes[x].length; y++) {
+            var n = getNeighbors(boxes, x, y);
+            if (n < 2 || n > 3)
+                newBoxes[x][y] = false;
+            if ((boxes[x][y] === true && n === 2) || n === 3)
+                newBoxes[x][y] = true;
+            if (boxes[x][y] === false && n === 3)
+                newBoxes[x][y] = true;
+        }
+    }
+    boxes = newBoxes.map(function (x) { return x; });
+};
+var drawBoxes = function () {
+    ctx.fillStyle = cellColor;
+    for (var x = 0; x < boxes.length; x++) {
+        for (var y = 0; y < boxes[x].length; y++) {
+            if (boxes[x][y] === true) {
+                ctx.fillRect(x * boxSize, y * boxSize, boxSize, boxSize);
+            }
+        }
+    }
+};
 var renderThings = function (timeStamp) {
     if (start === undefined) {
         start = timeStamp;
     }
-    if (timeStamp - lastFrame > 1) {
+    if (timeStamp - lastFrame > 50) {
         lastFrame = timeStamp;
-        if (row >= canvas.height)
-            direction = "up";
-        else if (row <= 0)
-            direction = "down";
-        if (direction === "up")
-            row -= 1;
-        if (direction === "down")
-            row += 1;
+        clear();
+        renderGrid();
+        calculateNewBoxes();
+        drawBoxes();
     }
-    clear();
-    ctx.beginPath();
-    ctx.moveTo(canvas.width / 2 - lineLength / 2, row);
-    ctx.lineTo(canvas.width / 2 + lineLength / 2, row);
-    ctx.strokeStyle = "turquoise";
-    ctx.stroke();
     window.requestAnimationFrame(renderThings);
+};
+var handleClick = function (event) {
+    setBoxes();
+    // console.log(event)
+    // calculateNewBoxes()
 };
 var initialize = function () {
     canvas = document.createElement("canvas");
@@ -47,6 +147,8 @@ var initialize = function () {
     canvas.id = "canvas";
     body.appendChild(canvas);
     ctx = canvas.getContext("2d");
+    canvas.addEventListener("click", handleClick);
+    setBoxes();
     window.requestAnimationFrame(renderThings);
 };
 document.body.onload = initialize;
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,15 @@
-let lineLength = window.innerWidth
+let lineLength = window.innerWidth / 3
 let start: number
-let canvas: HTMLCanvasElement, ctx: CanvasRenderingContext2D
+let canvas: HTMLCanvasElement
+let ctx: CanvasRenderingContext2D
 let lastFrame = 0
 let row = 0
-const backgroundColor = "#222222"
+const backgroundColor = "#000000"
+const cellColor = "turquoise"
 let direction = "down"
+const showGrid = false
+const boxSize = 3
+let boxes: boolean[][] = [[], []]
 
 const resizeCanvas = () => {
 	canvas.height = window.innerHeight - 4
@@ -18,27 +23,134 @@ const clear = () => {
 	ctx.fill()
 }
 
+const renderGrid = () => {
+	if (!showGrid) return
+	ctx.lineWidth = 1
+	ctx.strokeStyle = "#999999"
+	// draw vertical lines
+	for (let i = 0; i <= canvas.width; i += boxSize) {
+		ctx.beginPath()
+		ctx.moveTo(i, 0)
+		ctx.lineTo(i, canvas.height)
+		ctx.stroke()
+	}
+
+	// draw horizontal lines
+	for (let i = 0; i < canvas.height; i += boxSize) {
+		ctx.beginPath()
+		ctx.moveTo(0, i)
+		ctx.lineTo(canvas.width, i)
+		ctx.stroke()
+	}
+}
+
+const getNeighbors = (boxes, x, y) => {
+	let counter = 0
+
+	// left top
+	if (x > 0 && y > 0 && boxes[x - 1][y - 1]) counter++
+
+	// left middle
+	if (x > 0 && y < boxes[x].length - 1 && boxes[x - 1][y]) counter++
+
+	// left bottom
+	if (x > 0 && boxes[x - 1][y + 1]) counter++
+
+	// middle top
+	if (y > 0 && boxes[x][y - 1]) counter++
+
+	// middle bottom
+	if (y < boxes[x].length - 1 && boxes[x][y + 1]) counter++
+
+	// right top
+	if (y > 0 && x < boxes.length - 1 && boxes[x + 1][y - 1]) counter++
+
+	// right middle
+	if (x < boxes.length - 1 && boxes[x + 1][y]) counter++
+
+	// right bottom
+	if (x < boxes.length - 1 && y < boxes[x].length - 1 && boxes[x + 1][y + 1])
+		counter++
+
+	return counter
+}
+
+const setBoxes = () => {
+	const xBoxes = Math.floor(canvas.width / boxSize)
+	const yBoxes = Math.floor(canvas.height / boxSize)
+	for (let x = 0; x <= xBoxes; x++) {
+		boxes[x] = []
+		for (let y = 0; y <= yBoxes; y++) {
+			boxes[x][y] = false
+			if (Math.random() > 0.9) boxes[x][y] = true
+		}
+	}
+	// // boxes[Math.floor(xBoxes / 2) - 1][Math.floor(yBoxes / 2) - 1] = true
+	// // boxes[Math.floor(xBoxes / 2) - 1][Math.floor(yBoxes / 2)] = true
+	// boxes[Math.floor(xBoxes / 2) - 1][Math.floor(yBoxes / 2) + 1] = true
+
+	// boxes[Math.floor(xBoxes / 2)][Math.floor(yBoxes / 2) - 1] = true
+	// // boxes[Math.floor(xBoxes / 2)][Math.floor(yBoxes / 2)] = true
+	// boxes[Math.floor(xBoxes / 2)][Math.floor(yBoxes / 2) + 1] = true
+
+	// // boxes[Math.floor(xBoxes / 2) + 1][Math.floor(yBoxes / 2) - 1] = true
+	// boxes[Math.floor(xBoxes / 2) + 1][Math.floor(yBoxes / 2)] = true
+	// boxes[Math.floor(xBoxes / 2) + 1][Math.floor(yBoxes / 2) + 1] = true
+}
+
+const calculateNewBoxes = () => {
+	let newBoxes = []
+
+	// fill newBoxes with false
+	for (let i = 0; i < boxes.length; i++) {
+		newBoxes.push([])
+		for (let j = 0; j < boxes[i].length; j++) newBoxes[i].push(false)
+	}
+
+	for (let x = 0; x < boxes.length; x++) {
+		for (let y = 0; y < boxes[x].length; y++) {
+			const n = getNeighbors(boxes, x, y)
+			if (n < 2 || n > 3) newBoxes[x][y] = false
+			if ((boxes[x][y] === true && n === 2) || n === 3)
+				newBoxes[x][y] = true
+			if (boxes[x][y] === false && n === 3) newBoxes[x][y] = true
+		}
+	}
+
+	boxes = newBoxes.map((x) => x)
+}
+
+const drawBoxes = () => {
+	ctx.fillStyle = cellColor
+	for (let x = 0; x < boxes.length; x++) {
+		for (let y = 0; y < boxes[x].length; y++) {
+			if (boxes[x][y] === true) {
+				ctx.fillRect(x * boxSize, y * boxSize, boxSize, boxSize)
+			}
+		}
+	}
+}
+
 const renderThings = (timeStamp: number) => {
 	if (start === undefined) {
 		start = timeStamp
 	}
 
-	if (timeStamp - lastFrame > 1) {
+	if (timeStamp - lastFrame > 50) {
 		lastFrame = timeStamp
-		if (row >= canvas.height) direction = "up"
-		else if (row <= 0) direction = "down"
-		if (direction === "up") row -= 1
-		if (direction === "down") row += 1
+		clear()
+		renderGrid()
+		calculateNewBoxes()
+		drawBoxes()
 	}
 
-	clear()
-
-	ctx.beginPath()
-	ctx.moveTo(canvas.width / 2 - lineLength / 2, row)
-	ctx.lineTo(canvas.width / 2 + lineLength / 2, row)
-	ctx.strokeStyle = "turquoise"
-	ctx.stroke()
 	window.requestAnimationFrame(renderThings)
+}
+
+const handleClick = (event: PointerEvent) => {
+	setBoxes()
+	// console.log(event)
+	// calculateNewBoxes()
 }
 
 const initialize = () => {
@@ -50,6 +162,8 @@ const initialize = () => {
 	canvas.id = "canvas"
 	body.appendChild(canvas)
 	ctx = canvas.getContext("2d")
+	canvas.addEventListener("click", handleClick)
+	setBoxes()
 	window.requestAnimationFrame(renderThings)
 }
 
